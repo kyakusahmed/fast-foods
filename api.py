91610d060f97fc623398d8dbc4475c91c56a55e5
kyakulumbye ahmad <kyakusahmed@outlook.com>
2018-09-18 22:58:42 +0300

    [delivers #160600861] add POST an order api endpoint to the list of orders

--- api.py
+++ api.py
@@ -7,9 +7,17 @@ app2 = Flask(__name__)
 
 orders = Order( )
  
-@app2.route('/api/v1/orders/<int:id>', methods = ["GET"])
-def get_one(id):
-    return jsonify({'orders' : orders.get_order(id)}),200
+
+@app2.route('/api/v1/orders',methods = ['POST'])
+def addone():
+    data = request.get_json()
+    id = data["id"]
+    foodid = data["foodid"]
+    userid = data["userid"]
+    date = data["date"]
+    status = data["status"]
+    return jsonify({'orders' : orders.add_order(id, foodid, userid, date, status)}),201
+    
     
          
 if __name__ =="__main__":
