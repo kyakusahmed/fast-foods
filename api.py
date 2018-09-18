8a972f7d8a9ddc66c69be1d410f533334ea30a04
kyakulumbye ahmad <kyakusahmed@outlook.com>
2018-09-18 22:50:36 +0300

    [delivers  #160600672] add GET one order api endpoint

--- api.py
+++ api.py
@@ -7,12 +7,10 @@ app2 = Flask(__name__)
 
 orders = Order( )
  
-@app2.route('/api/v1/orders',methods = ['GET'])
-def return_all_orders( ):
-   return jsonify({'orders': orders.return_all_orders()}),200
-
-
-
+@app2.route('/api/v1/orders/<int:id>', methods = ["GET"])
+def get_one(id):
+    return jsonify({'orders' : orders.get_order(id)}),200
+    
          
 if __name__ =="__main__":
     app2.run(debug=True,port = 8080)
