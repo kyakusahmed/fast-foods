7efb087a43aa5f36d0ab3006c194665e28435b13
kyakulumbye ahmad <kyakusahmed@outlook.com>
2018-09-25 21:42:35 +0300

    [Fixes #160704070] fix assertion error

--- test_order.py
+++ test_order.py
@@ -33,7 +33,7 @@ class TestOrder(unittest.TestCase):
             "userid": "ahmed"
         }
         response = requests.post(base_url, params=data)
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json().get('orders')['userid'], "ahmed")
         """update status of a specific order"""
     def test_update_status(self):
         status = {"status":"pending"}
