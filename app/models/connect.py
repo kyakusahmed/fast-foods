2dc05b39f13976cf500cacfd8aa097e27c00c50b
kyakulumbye ahmad <kyakusahmed@outlook.com>
2018-10-18 20:25:44 +0300

    update connect.py file

--- app/models/connect.py
+++ app/models/connect.py
@@ -7,7 +7,7 @@ class DatabaseConnection:
     def __init__(self):
         try:
             # self.conn = psycopg2.connect(
-            # database="d4mdo559tepgh4", user="kovqxmgsijjgqs", password="a70ece5c8d7911994e3027aa8fef4d57a1fa07dcbc4c3208b19e165701a572e3", port="5432", host="ec2-54-243-147-162.compute-1.amazonaws.com"
+            # database="d4mdo559tepgh4", user="", password="a70ece5c8d7911994e3027aa8fef4d57a1fa07dcbc4c3208b19e165701a572e3", port="5432", host="ec2-54-243-147-162.compute-1.amazonaws.com"
             # )
             # self.conn.autocommit = True
             # self.cursor = self.conn.cursor()
